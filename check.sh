925c71e9812163bd90cfedff197d5e610349af83
Patrick Penner <penner@zbh.uni-hamburg.de>
2022-08-11 14:54:32 +0200

    Modified check.sh to only run once on error
    Change-Id: Ice6f3f78c3828054bc6348f73d6875f0303c0fc1

--- check.sh
+++ check.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # check the server is running correctly
 
-python3 manage.py check_server $FULL_URL
+# only run if there is not already an error
+if [ ! -s check_error.txt ]; then
+
+  python3 manage.py check_server $FULL_URL &> check_error.txt
+
+  if [ -s check_error.txt ]; then
+    cat check_error.txt
+  fi
+fi
